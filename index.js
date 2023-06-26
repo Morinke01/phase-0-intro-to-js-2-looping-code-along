b3cf9f8e52a25ffb2294d9bb76ef8c15ec25e26b
Morinke01 <ejulius250@gmail.com>
2023-06-26 22:49:11 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//function to display messages in an array
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+  }
+  
+  writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+  // Function to countdown numbers
+  const number = 10;
+  function countDown(number) {
+    while (number >= 0) {
+      return number;
+      number--;
+    }
+  }
+  countDown()
